def7575c4972d86ddae0c33cdbceaef89e530dda
David Batchelder <d.batchelder@btinternet.com>
2023-07-11 15:55:48 +0100

    Add files for running experiments

---
+++ experiment_args.py
@@ -0,0 +1,14 @@
+experiment_args = {
+    "num_hidden_units": [2,3],
+    "train_size": 4000,
+    "batch_size": 64,
+    "test_batch_size": 1000,
+    "epochs": 5,
+    "lr": 0.001,
+    "momentum": 0.95,
+    "gamma": 0.9,
+    "param_reuse": False,
+    "seed": 1,
+    "pre_transfer": False,
+    "log_interval": 20
+}
