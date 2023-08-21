dcd296d9048e5b165c83915a83d9d35148c9637c
David Batchelder <d.batchelder@btinternet.com>
2023-08-21 14:31:04 +0100

    Fix bug in extend_params

--- misc/experiment_scripts/landscape_script.py
+++ misc/experiment_scripts/landscape_script.py
@@ -208,7 +208,7 @@ def extend_params(params : list[torch.Tensor],
     assert params[0].shape == (new_model_size, 784), f"Expected {(new_model_size, 784)} but got {params[0].shape}"
     params[1] = torch.cat([params[1], torch.normal(0, 0.01, size=(hidden_unit_diff,), device=device)], dim=0)
     assert params[1].shape[0] == (new_model_size), f"Expected {new_model_size} but got {params[1].shape}"
-    params[2] = torch.cat([params[2], torch.normal(0, 0.1, size=(10, hidden_unit_diff), device=device)], dim=1)
+    params[2] = torch.cat([params[2], torch.normal(0, 0.01, size=(10, hidden_unit_diff), device=device)], dim=1)
     assert params[2].shape == (10, new_model_size), f"Expected {(10, new_model_size)} but got {params[2].shape}"
     return params
 
